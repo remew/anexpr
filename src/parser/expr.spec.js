3763ff687f1867c4b10d83d516672ad2bbe0f526
remew <foolishremew@gmail.com>
2018-08-22 01:43:34 +0900

    add complex expr test

--- src/parser/expr.spec.js
+++ src/parser/expr.spec.js
@@ -26,5 +26,52 @@ describe('ExprParser', () => {
     expect(p.parse({str: '1', pos: 0})).toEqual({type: 'NumberLiteral', value: 1});
     expect(p.parse({str: '$1', pos: 0})).toEqual({type: 'Variable', value: '$1'});
   });
+  it('should be parse complex expr', () => {
+    const p = new ExprParser();
+    expect(p.parse({str: '($1 + 2) * $2 + (($1 + $2) / 2)', pos: 0})).toEqual({
+      type: 'BinaryOperation',
+      left: {
+        type: 'BinaryOperation',
+        left: {
+          type: 'BinaryOperation',
+          left: {
+            type: 'Variable',
+            value: '$1',
+          },
+          operator: '+',
+          right: {
+            type: 'NumberLiteral',
+            value: 2,
+          },
+        },
+        operator: '*',
+        right: {
+          type: 'Variable',
+          value: '$2',
+        },
+      },
+      operator: '+',
+      right: {
+        type: 'BinaryOperation',
+        left: {
+          type: 'BinaryOperation',
+          left: {
+            type: 'Variable',
+            value: '$1',
+          },
+          operator: '+',
+          right: {
+            type: 'Variable',
+            value: '$2',
+          },
+        },
+        operator: '/',
+        right: {
+          type: 'NumberLiteral',
+          value: 2,
+        },
+      },
+    });
+  });
   // TODO: error test
 });
