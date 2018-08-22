0c5573e15642e52ab839871930b25d74698cd552
remew <foolishremew@gmail.com>
2018-08-23 01:43:23 +0900

    export main feature from 'index.js'

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // @flow
-console.log('Hello, world');
+import {Animated} from 'react-native';
+import CalculatorGenerator from './CalculatorGenerator';
+
+export default CalculatorGenerator({
+  add: (a, b) => Animated.add(a, b),
+  sub: (a, b) => Animated.subtract(a, b),
+  mul: (a, b) => Animated.multiply(a, b),
+  div: (a, b) => Animated.divide(a, b),
+});
